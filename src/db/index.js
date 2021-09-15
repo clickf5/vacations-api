0fbeb3aaf8aec96b8886367189cdcb153d431f70
Eugene Panin <paninhome@gmail.com>
2021-09-15 16:44:21 +0300

    fix: use environment variable

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,6 @@
 import mongoosePlugin from './mongoose.js';
 
-const uri = 'mongodb://mongo:27017/vacations';
+const uri = process.env.MONGODB_URL || 'mongodb://localhost:27017/vacations';
 
 export default (app) => {
   app.register(mongoosePlugin, { uri });
